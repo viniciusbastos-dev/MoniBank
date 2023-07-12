3d9201ff2cc3ebc135fe9441bf6d55e1dad24038
Vinícius Bastos <viniciusbsilva57@gmail.com>
2023-07-11 21:46:15 -0300

    Create the validaPrimeiroDigito and validaSegundoDigito function

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,8 +1,14 @@
 export default function verificarCPF(campo) {
-    const CPF = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(CPF);
-
-    console.log(validaNumerosRepetidos(CPF));
+    const cpf = campo.value.replace(/\.|-/g, "");
+    if (
+        validaNumerosRepetidos(cpf) ||
+        validaPrimeiroDigito(cpf) ||
+        validaSegundoDigito(cpf)
+    ) {
+        console.log("Esse cpf não existe");
+    } else {
+        console.log("Esse cpf existe");
+    }
 }
 
 function validaNumerosRepetidos(CPF) {
@@ -20,4 +26,36 @@ function validaNumerosRepetidos(CPF) {
     ];
 
     return numerosRepetidos.includes(CPF);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+    return soma != cpf[10];
 }
